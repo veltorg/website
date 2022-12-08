f0725afcd7c57dc19827bff794da18db586461b2
moripen <markusmoripen@gmail.com>
2022-12-08 23:41:08 +0100

    feat: add font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,12 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+          </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
