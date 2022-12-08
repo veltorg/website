d4e4fa0d67a259d4915e9d830b023a8208f39bfd
moripen <markusmoripen@gmail.com>
2022-12-08 22:57:16 +0100

    chore: copy paste from last project

---
+++ components/layout.tsx
@@ -0,0 +1,32 @@
+import { Flex } from "@chakra-ui/react"
+import Head from "next/head"
+import { ReactNode } from "react"
+import { Footer } from "./footer/footer"
+import { Header } from "./header/header"
+
+type Props = {
+    children?: ReactNode
+}
+
+export const Layout = ({
+    children,
+}: Props) => (
+    <Flex
+    sx={{
+      flexDirection: "column",
+      //gap: 0,
+      pb: 0,
+      maxWidth: "26.75em",
+    }}
+  >
+    <Header />
+    <Flex
+      sx={{
+        alignItems: "stretch",
+      }}
+    >
+      {children}
+    </Flex>
+      <Footer/>
+  </Flex>
+)
