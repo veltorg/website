2e791c7dc385856848b263b67261d39caaff94c1
moripen <markusmoripen@gmail.com>
2022-12-09 17:48:42 +0100

    fix: update syntax

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,21 +36,16 @@ const Home = () => (
         >
           <Image src="../snap.svg" sx={{}} height={128} width={90} alt={""} />
           <Flex sx={{ alignItems: "center", flexDirection: "column", gap: 0 }}>
-            <Text textStyle="header" sx={{ color: "blue00" }}>
+            <Text textStyle="header" color="blue00">
               Vi styrker mennesker til å oppnå mer
             </Text>
-            <Text
-              textStyle="normal"
-              sx={{
-                color: "blue00",
-              }}
-            >
+            <Text textStyle="normal" color="blue00">
               Vi gir deg verktøyene du trenger for å jobbe smart og effektivt i
               en ny digital hverdag.
             </Text>
           </Flex>
           <Link as={NextLink} href="/about" mr="auto">
-            <Text textStyle="link" sx={{ color: "blue00" }}>
+            <Text textStyle="link" color="blue00">
               Les mer om oss
             </Text>
           </Link>
@@ -66,16 +61,16 @@ const Home = () => (
       >
         <Image src="../squares.svg" sx={{}} width={110} height={121} alt={""} />
         <Flex sx={{ gap: 2, flexDirection: "column" }}>
-          <Text textStyle="header" sx={{ color: "green00" }}>
+          <Text textStyle="header" color="green00">
             Utvid, tilpass og skreddersy iterativt
           </Text>
-          <Text textStyle="normal" sx={{}}>
+          <Text textStyle="normal" color="blacktogreen">
             Vi elsker å skape fantastiske opplevelser, og vi innser at alle har
             forskjellige behov. Med oss på laget kan du utvide, tilpasse og
             skreddersky mens du går.
           </Text>
           <Link as={NextLink} href="/about">
-            <Text textStyle="link" sx={{ color: "black" }}>
+            <Text textStyle="link" color="blacktogreen">
               Les mer om vår kompetanse
             </Text>
           </Link>
@@ -90,20 +85,20 @@ const Home = () => (
           gap: 2,
         }}
       >
-        <Text textStyle="header" sx={{ color: "green10noswap" }}>
+        <Text textStyle="header" color="green10noswap">
           Lorem ipsum sit amet
         </Text>
-        <Text textStyle="normal" sx={{ color: "green10noswap" }}>
+        <Text textStyle="normal" color="green10noswap">
           Aliquam pellentesque urna lacinia, finibus tellus non, aliquam urna.
           Phasellus varius aliquet massa, id tempus dui aliquet at.
         </Text>
-        <Text textStyle="normal" sx={{ color: "green10noswap" }}>
+        <Text textStyle="normal" color="green10noswap">
           Aenean aliquam sed ante nec aliquam. Pellentesque habitant morbi
           tristique senectus et netus et malesuada fames ac turpis egestas. In
           sit amet metus efficitur, cursus sapien maximus, semper purus.
         </Text>
         <Link href="/about" mr="auto">
-          <Text textStyle="link" sx={{ color: "green10noswap" }}>
+          <Text textStyle="link" color="green10noswap">
             Les mer om oss
           </Text>
         </Link>
