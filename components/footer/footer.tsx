274b234b4898fecedb2061185d2e9b443db82861
moripen <markusmoripen@gmail.com>
2022-12-09 17:50:18 +0100

    feat: logos different for each colormode

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,19 +1,26 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Freshworks } from "./freshworks";
+import { FreshworksDark } from "./freshworks-dark";
 import { Switches } from "./switch";
 
-export const Footer: React.FC = () => (
-  <Flex sx={{ flexDirection: "column", p: 3, gap: 4 }}>
-    <Flex sx={{ alignItems: "center", justifyContent: "center", gap: 1 }}>
-      <Image src="freshworks-logo.svg" />
+export const Footer: React.FC = () => {
+  const { colorMode, setColorMode } = useColorMode();
+  const freshLogo = colorMode === "light" ? <Freshworks /> : <FreshworksDark />;
+  //const switchInput = colorMode === "light" ? <Switches/> : <SwitchesDark>
+  return (
+    <Flex sx={{ flexDirection: "column", p: 3, gap: 4 }}>
+      <Flex sx={{ alignItems: "center", justifyContent: "center", gap: 1 }}>
+        {freshLogo}
+      </Flex>
+      <Flex
+        sx={{
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Switches />
+      </Flex>
     </Flex>
-    <Flex
-      sx={{
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Switches />
-    </Flex>
-  </Flex>
-);
+  );
+};
