2c6ea69d7110323b652cee07a65f75951e2ba21e
moripen <markusmoripen@gmail.com>
2023-01-05 17:19:43 +0100

    fix: remove griditem

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,107 +27,99 @@ const Home = () => (
     </Show>
     <Show below="md">
       <Container width="100%" px={3} py={3}>
-        <GridItem>
+        <Flex
+          sx={{
+            backgroundColor: 'pink00',
+            flexDirection: 'column',
+            borderRadius: 'md',
+          }}
+        >
+          <Image
+            src={'../tablemeet.png'}
+            width={'100%'}
+            alt={''}
+            borderTopRadius="md"
+          />
           <Flex
             sx={{
-              backgroundColor: 'pink00',
-              flexDirection: 'column',
-              borderRadius: 'md',
-            }}
-          >
-            <Image
-              src={'../tablemeet.png'}
-              width={'100%'}
-              alt={''}
-              borderTopRadius="md"
-            />
-            <Flex
-              sx={{
-                p: 3,
-                alignItems: 'center',
-                flexDirection: 'column',
-                gap: 2,
-              }}
-            >
-              <Image src="./snap.svg" alt="" width="20%" />
-              <Flex
-                sx={{ flexDirection: 'column', gap: 0, alignItems: 'center' }}
-              >
-                <Text textStyle="header" color="blue00">
-                  Vi styrker mennesker til å oppnå mer
-                </Text>
-                <Text textStyle="normal" color="blue00">
-                  Vi gir deg verktøyene du trenger for å jobbe smart og
-                  effektivt i en ny digital hverdag.
-                </Text>
-              </Flex>
-              <Link as={NextLink} href="/about">
-                <Text textStyle="link" color="blue00">
-                  Les mer om oss
-                </Text>
-              </Link>
-            </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Flex
-            sx={{
-              alignItems: 'center',
               p: 3,
+              alignItems: 'center',
               flexDirection: 'column',
               gap: 2,
             }}
           >
-            <Image src="../squares.svg" sx={{}} width="20%" alt={''} />
+            <Image src="./snap.svg" alt="" width="20%" />
             <Flex
-              sx={{ gap: 2, flexDirection: 'column', alignItems: 'center' }}
+              sx={{ flexDirection: 'column', gap: 0, alignItems: 'center' }}
             >
-              <Text textStyle="header" color="green00" alignSelf="center">
-                Utvid, tilpass og skreddersy iterativt
+              <Text textStyle="header" color="blue00">
+                Vi styrker mennesker til å oppnå mer
               </Text>
-              <Text textStyle="normal" color="blacktogreen">
-                Vi elsker å skape fantastiske opplevelser, og vi innser at alle
-                har forskjellige behov. Med oss på laget kan du utvide, tilpasse
-                og skreddersky mens du går.
+              <Text textStyle="normal" color="blue00">
+                Vi gir deg verktøyene du trenger for å jobbe smart og effektivt
+                i en ny digital hverdag.
               </Text>
-              <Link as={NextLink} href="/about">
-                <Text textStyle="link" color="blacktogreen">
-                  Les mer om vår kompetanse
-                </Text>
-              </Link>
             </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Flex
-            sx={{
-              flexDirection: 'column',
-              p: 3,
-              backgroundColor: 'green00noswap',
-              borderRadius: 'md',
-              alignItems: 'center',
-              gap: 2,
-            }}
-          >
-            <Text textStyle="header" color="green10noswap">
-              Lorem ipsum sit amet
-            </Text>
-            <Text textStyle="normal" color="green10noswap">
-              Aliquam pellentesque urna lacinia, finibus tellus non, aliquam
-              urna. Phasellus varius aliquet massa, id tempus dui aliquet at.
-            </Text>
-            <Text textStyle="normal" color="green10noswap">
-              Aenean aliquam sed ante nec aliquam. Pellentesque habitant morbi
-              tristique senectus et netus et malesuada fames ac turpis egestas.
-              In sit amet metus efficitur, cursus sapien maximus, semper purus.
-            </Text>
-            <Link href="/about">
-              <Text textStyle="link" color="green10noswap">
+            <Link as={NextLink} href="/about">
+              <Text textStyle="link" color="blue00">
                 Les mer om oss
               </Text>
             </Link>
           </Flex>
-        </GridItem>
+        </Flex>
+        <Flex
+          sx={{
+            alignItems: 'center',
+            p: 3,
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <Image src="../squares.svg" sx={{}} width="20%" alt={''} />
+          <Flex sx={{ gap: 2, flexDirection: 'column', alignItems: 'center' }}>
+            <Text textStyle="header" color="green00" alignSelf="center">
+              Utvid, tilpass og skreddersy iterativt
+            </Text>
+            <Text textStyle="normal" color="blacktogreen">
+              Vi elsker å skape fantastiske opplevelser, og vi innser at alle
+              har forskjellige behov. Med oss på laget kan du utvide, tilpasse
+              og skreddersky mens du går.
+            </Text>
+            <Link as={NextLink} href="/about">
+              <Text textStyle="link" color="blacktogreen">
+                Les mer om vår kompetanse
+              </Text>
+            </Link>
+          </Flex>
+        </Flex>
+        <Flex
+          sx={{
+            flexDirection: 'column',
+            p: 3,
+            backgroundColor: 'green00noswap',
+            borderRadius: 'md',
+            alignItems: 'center',
+            gap: 2,
+          }}
+        >
+          <Text textStyle="header" color="green10noswap">
+            Lorem ipsum sit amet
+          </Text>
+          <Text textStyle="normal" color="green10noswap">
+            Aliquam pellentesque urna lacinia, finibus tellus non, aliquam urna.
+            Phasellus varius aliquet massa, id tempus dui aliquet at.
+          </Text>
+          <Text textStyle="normal" color="green10noswap">
+            Aenean aliquam sed ante nec aliquam. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas. In
+            sit amet metus efficitur, cursus sapien maximus, semper purus.
+          </Text>
+          <Link href="/about">
+            <Text textStyle="link" color="green10noswap">
+              Les mer om oss
+            </Text>
+          </Link>
+        </Flex>
       </Container>
     </Show>
   </Layout>
