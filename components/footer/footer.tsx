339bde77f59be15611fe92abae3a02864ff89286
moripen <markusmoripen@gmail.com>
2023-01-05 23:22:47 +0100

    feat: desktopfooter start

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,13 +1,22 @@
-import { Flex, Image, useColorMode } from '@chakra-ui/react';
+import { Container, Flex, Image, Show, useColorMode } from '@chakra-ui/react';
 import { Freshworks } from './elements/freshworks';
 import { FreshworksDark } from './elements/freshworks-dark';
 import { Switches } from './elements/switch';
+import { LogoDark } from '../header/elements/logo-dark';
+import { Logo } from '../header/elements/logo';
 
 export const Footer: React.FC = () => {
   const { colorMode, setColorMode } = useColorMode();
   const freshLogo = colorMode === 'light' ? <Freshworks /> : <FreshworksDark />;
-  //const switchInput = colorMode === "light" ? <Switches/> : <SwitchesDark>
+  const logoInput = colorMode === 'light' ? <Logo /> : <LogoDark/>
   return (
+    <Flex width='100%' m="0rem" p="0rem" flexDirection='column'>
+      <Show above="md">
+        <Flex backgroundColor='green10' width='100%' height='30rem'>
+          poaw
+        </Flex>
+      </Show>
+    <Show below="md">
     <Flex sx={{ flexDirection: 'column', p: 3, gap: 4, mt: 2 }}>
       <Flex sx={{ alignItems: 'center', justifyContent: 'center', gap: 1 }}>
         {freshLogo}
@@ -21,6 +30,8 @@ export const Footer: React.FC = () => {
       >
         <Switches />
       </Flex>
+    </Flex>
+    </Show>
     </Flex>
   );
 };
