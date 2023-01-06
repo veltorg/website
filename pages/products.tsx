f5d48c964e0fed0959efc10103711d02a7fcdaca
moripen <markusmoripen@gmail.com>
2023-01-06 03:14:42 +0100

    feat: products page tabs

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,4 +1,4 @@
-import { Flex, FormControl, FormHelperText, FormLabel, Image, Input, InputGroup, InputLeftElement, Show, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+import { Button, Flex, FormControl, FormHelperText, FormLabel, Image, Input, InputGroup, InputLeftElement, Show, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text, propNames } from "@chakra-ui/react";
 import { Layout } from "../components/layout";
 import { EmailIcon, PhoneIcon, SearchIcon, SettingsIcon } from "@chakra-ui/icons";
 
@@ -30,7 +30,7 @@ const Products: React.FC = () => {
                             <TabPanels>
                                 <TabPanel>
                                     <Flex flexDirection="row" gap={3}>
-                                        <Flex flexDirection="column" width='50%' gap={2}>
+                                        <Flex flexDirection="column" width='50%' gap={2}  justifyContent="center">
                                             <Text fontSize="5xl" lineHeight='3.5rem' fontWeight="bold">
                                                 Hank dem inn og behold dem.
                                             </Text>
@@ -66,18 +66,146 @@ const Products: React.FC = () => {
                                                     <Input type='tel' placeholder="Telefonnummer" isRequired/>
                                                 </InputGroup>
                                                 </Stack>
+                                                <Button type='submit' width="100%" bgColor="green10" mt={2}>
+                                                    Registrer Gratis
+                                                </Button>
                                             </FormControl>
                                         </Flex>
                                     </Flex>
                                 </TabPanel>
                                 <TabPanel>
-                                    
+                                    <Flex flexDirection="row" gap={3}>
+                                        <Flex flexDirection="column" width='50%' gap={2}  justifyContent="center">
+                                            <Text fontSize="5xl" lineHeight='3.5rem' fontWeight="bold">
+                                                Alt i samme helpdesk
+                                            </Text>
+                                            <Text fontSize="xl">
+                                            Freshdesk er en skybasert programvare for kundeservice, som samler sammen samtaler fra mange ulike kanaler, som e-post, telefon, chat og sosiale medier. Freshdesk gjøre det mulig for team å samarbeide på tvers av funksjoner for å gi eksepsjonell kundesupport. 
+                                            </Text>
+                                        </Flex>
+                                        <Flex flexDirection="column" width="50%" gap={2}>
+                                            <Text textAlign="center" fontSize='3xl' fontWeight="bold">
+                                                Start din 21-dagers gratis prøveperiode.
+                                            </Text>
+                                            <FormControl>
+                                                <Stack direction="row" spacing={2}>
+                                                    <InputGroup alignItems="center">
+                                                        <InputLeftElement children={<SearchIcon/>}/>
+                                                        <Input type='name' placeholder="Fornavn" isRequired/>
+                                                    </InputGroup>
+                                                    <InputGroup alignItems="center">
+                                                        <Input type='name' placeholder="Etternavn" isRequired/>
+                                                    </InputGroup>
+                                                </Stack>
+                                                <Stack direction="column" spacing={0} alignItems="center">
+                                                <InputGroup >
+                                                    <InputLeftElement children={<EmailIcon/>} />
+                                                    <Input type='email' placeholder="E-post" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<SettingsIcon/>} />
+                                                    <Input type='name' placeholder="Bedrift" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<PhoneIcon/>} />
+                                                    <Input type='tel' placeholder="Telefonnummer" isRequired/>
+                                                </InputGroup>
+                                                </Stack>
+                                                <Button type='submit' width="100%" bgColor="green10" mt={2}>
+                                                    Registrer Gratis
+                                                </Button>
+                                            </FormControl>
+                                        </Flex>
+                                    </Flex>
                                 </TabPanel>
                                 <TabPanel>
-                                
+                                    <Flex flexDirection="row" gap={3}>
+                                        <Flex flexDirection="column" width='50%' gap={2}  justifyContent="center">
+                                            <Text fontSize="5xl" lineHeight='3.5rem' fontWeight="bold">
+                                            CRM-løsningen ditt salg og markedsteam vil elske
+                                            </Text>
+                                            <Text fontSize="xl">
+                                            Freshworks CRM bruker AI, automasjoner og sømløse integrasjoner for å gi deg de verktøyene ditt salgs- og markedsteam behøver for å jobbe bedre. 
+                                            </Text>
+                                        </Flex>
+                                        <Flex flexDirection="column" width="50%" gap={2}>
+                                            <Text textAlign="center" fontSize='3xl' fontWeight="bold">
+                                                Start din 30-dagers gratis prøveperiode.
+                                            </Text>
+                                            <FormControl>
+                                                <Stack direction="row" spacing={2}>
+                                                    <InputGroup alignItems="center">
+                                                        <InputLeftElement children={<SearchIcon/>}/>
+                                                        <Input type='name' placeholder="Fornavn" isRequired/>
+                                                    </InputGroup>
+                                                    <InputGroup alignItems="center">
+                                                        <Input type='name' placeholder="Etternavn" isRequired/>
+                                                    </InputGroup>
+                                                </Stack>
+                                                <Stack direction="column" spacing={0} alignItems="center">
+                                                <InputGroup >
+                                                    <InputLeftElement children={<EmailIcon/>} />
+                                                    <Input type='email' placeholder="E-post" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<SettingsIcon/>} />
+                                                    <Input type='name' placeholder="Bedrift" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<PhoneIcon/>} />
+                                                    <Input type='tel' placeholder="Telefonnummer" isRequired/>
+                                                </InputGroup>
+                                                </Stack>
+                                                <Button type='submit' width="100%" bgColor="green10" mt={2}>
+                                                    Registrer Gratis
+                                                </Button>
+                                            </FormControl>
+                                        </Flex>
+                                    </Flex>
                                 </TabPanel>
                                 <TabPanel>
-                                
+                                    <Flex flexDirection="row" gap={3}>
+                                        <Flex flexDirection="column" width='50%' gap={2}  justifyContent="center">
+                                            <Text fontSize="5xl" lineHeight='3.5rem' fontWeight="bold">
+                                            Alt-i-en IT-servicedesk
+                                            </Text>
+                                            <Text fontSize="xl">
+                                            Freshservice er en skybasert og moderne IT service desk, som er enkel å bruke og lett å konfigurere.</Text>
+                                        </Flex>
+                                        <Flex flexDirection="column" width="50%" gap={2}>
+                                            <Text textAlign="center" fontSize='3xl' fontWeight="bold">
+                                                Start din 21-dagers gratis prøveperiode.
+                                            </Text>
+                                            <FormControl>
+                                                <Stack direction="row" spacing={2}>
+                                                    <InputGroup alignItems="center">
+                                                        <InputLeftElement children={<SearchIcon/>}/>
+                                                        <Input type='name' placeholder="Fornavn" isRequired/>
+                                                    </InputGroup>
+                                                    <InputGroup alignItems="center">
+                                                        <Input type='name' placeholder="Etternavn" isRequired/>
+                                                    </InputGroup>
+                                                </Stack>
+                                                <Stack direction="column" spacing={0} alignItems="center">
+                                                <InputGroup >
+                                                    <InputLeftElement children={<EmailIcon/>} />
+                                                    <Input type='email' placeholder="E-post" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<SettingsIcon/>} />
+                                                    <Input type='name' placeholder="Bedrift" isRequired/>
+                                                </InputGroup>
+                                                <InputGroup >
+                                                    <InputLeftElement children={<PhoneIcon/>} />
+                                                    <Input type='tel' placeholder="Telefonnummer" isRequired/>
+                                                </InputGroup>
+                                                </Stack>
+                                                <Button type='submit' width="100%" bgColor="green10" mt={2}>
+                                                    Registrer Gratis
+                                                </Button>
+                                            </FormControl>
+                                        </Flex>
+                                    </Flex>
                                 </TabPanel>
                             </TabPanels>
                         </Tabs>
