8c9e3bbfc64d60e01c5ea24ac6a38c0e4b8137c5
moripen <markusmoripen@gmail.com>
2023-01-11 14:25:50 +0100

    feat: update gridinputs

--- components/freshgrid-input.ts
+++ components/freshgrid-input.ts
@@ -10,24 +10,24 @@ export const freshgridInput: Input[] = [
         href: 'https://velt.no/produkter/freshdesk/'
     },
     {
-        type: 'Kundesenterløsning',
-        img: '../freshdesk-logo.svg',
-        name: 'Freshdesk',
-        description: 'Samle dine henvendelser fra alle kanaler og organiser de for en mer effektiv og produktiv kundeservice.',
-        href: 'https://velt.no/produkter/freshdesk/'
+        type: 'Salgs- og CRM-løsning',
+        img: '../freshsales-logo.svg',
+        name: 'Freshsales',
+        description: 'Øk produktiviteten til ditt salgsteam med AI-basert leadscoring, telefon og e-postløsninger og lead scoring. ',
+        href: 'https://velt.no/produkter/freshsales/'
     },
     {
-        type: 'Kundesenterløsning',
-        img: '../freshdesk-logo.svg',
-        name: 'Freshdesk',
-        description: 'Samle dine henvendelser fra alle kanaler og organiser de for en mer effektiv og produktiv kundeservice.',
-        href: 'https://velt.no/produkter/freshdesk/'
+        type: 'Chat og meldingsløsning',
+        img: '../freshchat-logo.svg',
+        name: 'Freshchat',
+        description: 'Integrer chat-funksjon på hjemmesiden og få henvendelser for både salg, support og markedsføring.',
+        href: 'https://velt.no/produkter/freshchat/'
     },
     {
-        type: 'Kundesenterløsning',
-        img: '../freshdesk-logo.svg',
-        name: 'Freshdesk',
-        description: 'Samle dine henvendelser fra alle kanaler og organiser de for en mer effektiv og produktiv kundeservice.',
-        href: 'https://velt.no/produkter/freshdesk/'
+        type: 'IT-supportløsning',
+        img: '../freshservice-logo.svg',
+        name: 'Freshservice',
+        description: 'Moderniser IT og andre forretningsfunksjoner med en brukervennlig, enkel og smart IT-servicedesk-løsning i skyen.',
+        href: 'https://velt.no/produkter/freshservice/'
     },
 ]
