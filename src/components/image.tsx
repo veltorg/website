126e6e12a3e36a2f24d6e965622fce1f98896697
Simen A. W. Olsen <so@bjerk.io>
2023-01-27 10:13:35 +0100

    rename/move image component

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,5 +1,5 @@
-import { chakra } from "@chakra-ui/react";
-import NextImage from "next/image";
+import { chakra } from '@chakra-ui/react';
+import NextImage from 'next/image';
 
 export const Image = chakra(NextImage, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop),
