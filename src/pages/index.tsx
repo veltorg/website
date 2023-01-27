65b82a58cb5901b50a59cbae8626949c8486abf5
Simen A. W. Olsen <so@bjerk.io>
2023-01-27 10:14:24 +0100

    style: fix eslint issues and run prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
-import { AspectRatio, Box, Center, Container } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import React from 'react';
 import veltHero from '../assets/velt-hero.jpg';
+import { Image } from '../components/image';
 import { Layout } from '../components/layout';
-import { Image } from '../utils';
 
 const Home: NextPage = () => (
   <Layout>
     <Container sx={{ maxW: '1695px' }}>
-        <Image
-          src={veltHero}
-          sx={{ objectFit: 'cover', maxW: '1695px', maxH: '612px' }}
-          alt="people around table"
-          placeholder="blur"
-          quality={100}
-        />
+      <Image
+        src={veltHero}
+        sx={{ objectFit: 'cover', maxW: '1695px', maxH: '612px' }}
+        alt="people around table"
+        placeholder="blur"
+        quality={100}
+      />
     </Container>
   </Layout>
 );
