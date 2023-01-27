58cce87087a9732b62f5841bd8eaf480e91aa596
Simen A. W. Olsen <so@bjerk.io>
2023-01-27 20:12:46 +0100

    add support for static generation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withExportImages = require('next-export-optimize-images');
+
+const nextConfig = withExportImages({
   reactStrictMode: true,
   swcMinify: true,
-};
+});
 
 module.exports = nextConfig;
